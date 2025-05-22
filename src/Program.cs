5a287e7192bd924120a39a6e9af7f165a6ce90a4
Joe Gallagher <github@pingmint.net>
2025-05-22 12:57:13 -0400

    set line spacing

--- src/Program.cs
+++ src/Program.cs
@@ -13,7 +13,7 @@ Console.WriteLine($"DPI: {dpi}");
 
 var fontSize = 16;
 var font = Raylib.LoadFontEx("/System/Library/Fonts/Monaco.ttf", (int)(fontSize * dpi.Y), [], 512);
-
+// var font = Raylib.LoadFontEx("/System/Library/Fonts/Supplemental/Times New Roman.ttf", (int)(fontSize * dpi.Y), [], 512);
 Raylib.SetTextureFilter(font.Texture, TextureFilter.Bilinear);
 
 var chars = File.ReadAllText("Program.cs");
@@ -24,10 +24,27 @@ while (!Raylib.WindowShouldClose())
     Raylib.BeginDrawing();
     Raylib.ClearBackground(new Color(32, 32, 32, 255));
 
-    // draw text
     var scroll = Raylib.GetMouseWheelMoveV();
     vec4 += scroll * fontSize;
-    Raylib.DrawTextEx(font, chars, vec4, fontSize, 1, Color.LightGray);
+
+    if (Raylib.IsKeyDown(KeyboardKey.Space))
+    {
+        // draw text
+        var lines = chars.Split('\n');
+        var vec5 = vec4;
+        foreach (var line in lines)
+        {
+            var size = Raylib.MeasureTextEx(font, line, fontSize, 1);
+            Raylib.DrawTextEx(font, line, vec5, fontSize, 1, Color.White);
+            vec5.Y += size.Y;
+        }
+    }
+    else
+    {
+        // draw text
+        Raylib.SetTextLineSpacing(fontSize);
+        Raylib.DrawTextEx(font, chars, vec4, fontSize, 1, Color.LightGray);
+    }
 
     // fps
     var fps = Raylib.GetFPS();
@@ -35,9 +52,21 @@ while (!Raylib.WindowShouldClose())
     var fpsSize = Raylib.MeasureTextEx(font, fpsString, fontSize, 1);
     var fpsVec = new Vector2(Raylib.GetScreenWidth(), 0);
     fpsVec.X -= fpsSize.X + 5;
-    Raylib.DrawTextEx(font, fpsString, fpsVec, fontSize, 1, Color.LightGray);
+    Raylib.DrawTextEx(font, fpsString, fpsVec, fontSize, 1, Color.RayWhite);
+
+    // dpi
+    var dpiString = $"DPI: {dpi}";
+    var dpiSize = Raylib.MeasureTextEx(font, dpiString, fontSize, 1);
+    var dpiVec = new Vector2(Raylib.GetScreenWidth(), fpsVec.Y + fpsSize.Y);
+    dpiVec.X -= dpiSize.X + 5;
+    Raylib.DrawTextEx(font, dpiString, dpiVec, fontSize, 1, Color.RayWhite);
 
     Raylib.EndDrawing();
+
+    if (Raylib.IsKeyPressed(KeyboardKey.Escape))
+    {
+        break;
+    }
 }
 
 Raylib.CloseWindow();
