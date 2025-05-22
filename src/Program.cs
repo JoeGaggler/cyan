338c423ca6bfab8f3bcee882807ead3f6f774221
Joe Gallagher <github@pingmint.net>
2025-05-21 20:32:09 -0400

    scrolling text

--- src/Program.cs
+++ src/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System.Numerics;
 using Raylib_cs;
 
-Raylib.SetTargetFPS(61);
+Raylib.SetTargetFPS(60 + 1);
 Raylib.SetConfigFlags(ConfigFlags.ResizableWindow | ConfigFlags.HighDpiWindow | ConfigFlags.VSyncHint);
 Raylib.InitWindow(1027, 768, "Hello World");
 Raylib.SetWindowMinSize(100, 100);
@@ -16,50 +16,26 @@ var font = Raylib.LoadFontEx("/System/Library/Fonts/Monaco.ttf", (int)(fontSize 
 
 Raylib.SetTextureFilter(font.Texture, TextureFilter.Bilinear);
 
-var chars = """
-abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 !@#$%^&*()_+[]{}|;':\",./<>? 
-""";
+var chars = File.ReadAllText("Program.cs");
 
-float x = 0;
-string buff = string.Empty;
-buff = chars;
+var vec4 = new Vector2(0, 0);
 while (!Raylib.WindowShouldClose())
 {
     Raylib.BeginDrawing();
     Raylib.ClearBackground(new Color(32, 32, 32, 255));
 
-    var measure = Raylib.MeasureTextEx(font, "Hello, world!", fontSize, 0);
+    // draw text
+    var scroll = Raylib.GetMouseWheelMoveV();
+    vec4 += scroll * fontSize;
+    Raylib.DrawTextEx(font, chars, vec4, fontSize, 1, Color.LightGray);
 
-    Vector2 vec0 = new Vector2(0, 0);
-    Vector2 vec1 = new Vector2(10, 0);
-    Vector2 vec2 = new Vector2(10, 25);
-    Vector2 vec3 = new Vector2(x, 50);
-
-    Raylib.DrawTextEx(font, $"Hello, world! {measure} {Raylib.GetTime():0.000}", vec1, fontSize, 1, Color.LightGray);
-    Raylib.DrawTextEx(font, $"{Raylib.GetFPS()}", vec2, fontSize, 1, Color.LightGray);
-    Raylib.DrawTextPro(font, $"{x:0.000}", vec3, vec0, 0, fontSize, 1, Color.LightGray);
-
-    x += 100 * Raylib.GetFrameTime() * 0.2f;
-    if (x > 400) x = 0;
-
-    var vec4 = new Vector2(0, 60);
-
-    Raylib.BeginScissorMode(100, 100, 1000, 1000);
-
-    var vec5 = new Vector2(0 + x, 60);
-    var vec6 = new Vector2(900 + x, 60);
-    var vec7 = new Vector2(1800 + x, 60);
-    for (int j = 0; j < 100; j++)
-    {
-        Raylib.DrawTextEx(font, buff, vec5, fontSize, 1, Color.LightGray);
-        Raylib.DrawTextEx(font, buff, vec6, fontSize, 1, Color.LightGray);
-        Raylib.DrawTextEx(font, buff, vec7, fontSize, 1, Color.LightGray);
-        vec5.Y += fontSize;
-        vec6.Y += fontSize;
-        vec7.Y += fontSize;
-    }
-
-    Raylib.EndScissorMode();
+    // fps
+    var fps = Raylib.GetFPS();
+    var fpsString = $"FPS: {fps}";
+    var fpsSize = Raylib.MeasureTextEx(font, fpsString, fontSize, 1);
+    var fpsVec = new Vector2(Raylib.GetScreenWidth(), 0);
+    fpsVec.X -= fpsSize.X + 5;
+    Raylib.DrawTextEx(font, fpsString, fpsVec, fontSize, 1, Color.LightGray);
 
     Raylib.EndDrawing();
 }
